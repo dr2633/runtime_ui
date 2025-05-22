4f6a24f2a395c0a76373994a2a337b996587604c
Derek Rosenzweig <dr2633@nyu.edu>
2025-05-21 17:30:32 -0700

    Renaming config as CommonJS

--- index.tsx
+++ index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { CalendarProvider } from './contexts/CalendarContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CalendarProvider>
+        <App />
+      </CalendarProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
