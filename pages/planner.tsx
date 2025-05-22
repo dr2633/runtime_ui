4cb2b0c9299f2701a3534daa096eb28ddc85901d
Derek Rosenzweig <dr2633@nyu.edu>
2025-05-22 15:20:50 -0700

    Adding planner interface for pages

---
+++ pages/planner.tsx
@@ -0,0 +1,13 @@
+// pages/planner.tsx
+import React from 'react';
+import { SimulationCalendar } from '@/components/calendar/SimulationCalendar';
+
+const PlannerPage: React.FC = () => {
+  return (
+    <div className="min-h-screen w-full bg-black text-white font-sans">
+      <SimulationCalendar />
+    </div>
+  );
+};
+
+export default PlannerPage;
